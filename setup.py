1f891e5c2f3cd44eb1d5513657040fb19e192a19
Babusjc <babu.sjc38@gmail.com>
2025-08-18 12:21:40 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meteorologia_app",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # listar dependências aqui
+    ],
+)
